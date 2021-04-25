de4adb21f35fa1435e561e316237efb9f2a264ad
Alexandre Vanderlei <72346464+alexandre-dev-73@users.noreply.github.com>
2021-04-25 20:09:43 -0300

    pontos aletorios

---
+++ rw_visual.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+#Continua criando novos passeios enquanto o programa estiver ativo
+while True:
+    #cria u passeio aleatório e plota os pontos
+    rw = RandomWalk()
+    rw.fill_walk()
+    point_numbers = list(range(rw.num_points))
+    #Remove os eixos
+    plt.axes().get_yaxis().set_visible(False)
+    #plt.axes().get_xaxis().set_visible(True)
+
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+
+    #Enfatiza o primerio eo último ponto
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
