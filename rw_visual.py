f8fc53cd8835c6f02dc7594c89c1b418c3947cf3
Alexandre Vanderlei <72346464+alexandre-dev-73@users.noreply.github.com>
2021-05-02 16:42:47 -0300

    alterações em pontos aleatorios

--- rw_visual.py
+++ rw_visual.py
@@ -5,14 +5,20 @@ from random_walk import RandomWalk
 #Continua criando novos passeios enquanto o programa estiver ativo
 while True:
     #cria u passeio aleatório e plota os pontos
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
+
+    #Define o tamanho da janela de plotagem
+    plt.figure(dpi=128, figsize=(10, 6))
+    #Plotando os pontos e mostra o grafico
     point_numbers = list(range(rw.num_points))
     #Remove os eixos
     plt.axes().get_yaxis().set_visible(False)
     #plt.axes().get_xaxis().set_visible(True)
 
-    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+   # plt.plot(rw.x_values, rw.y_values, 'go--', linewidth=2, markersize=0)
+
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
 
     #Enfatiza o primerio eo último ponto
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
